3f8bf418b1f19caea865a45a0d5ceba533a5c13c
FelipeQH29 <fquesada10287@ufide.ac.cr>
2025-05-21 19:25:14 -0600

    Commit1
    mi primer commit

---
+++ Tienda_Felipe/src/main/java/Tienda/web/TiendaFelipeApplication.java
@@ -0,0 +1,13 @@
+package Tienda.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaFelipeApplication.class, args);
+	}
+
+}
