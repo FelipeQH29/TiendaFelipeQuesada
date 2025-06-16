3b1211e6d37f798efa83b21fff50c2a4bc640472
FelipeQH29 <fquesada10287@ufide.ac.cr>
2025-06-15 19:23:00 -0600

    semana 4

---
+++ Tienda_Felipe/src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,10 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+
+    // Se obtiene un listado de categorías en un List
+    public List<Categoria> getCategorias(boolean activos);
+}
