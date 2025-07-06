861d3f30820ec88d7f08473ba3fe27369ff29914
FelipeQH29 <fquesada10287@ufide.ac.cr>
2025-07-05 23:13:57 -0600

    Revert "Merge pull request #2 from FelipeQH29/revert-1-FelipeQuesadaH"
    This reverts commit 46feae269b60c6cae39438dd72af7443fee093d3, reversing
    changes made to 87f0612d762830c832b2a6ef42666f392b7cc5be.

---
+++ Tienda_Felipe/src/main/java/com/tienda/TiendaFelipeApplication.java
@@ -0,0 +1,12 @@
+
+package com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFelipeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaFelipeApplication.class, args);
+    }
+}
---
+++ Tienda_Felipe/src/test/java/Tienda/web/TiendaFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
