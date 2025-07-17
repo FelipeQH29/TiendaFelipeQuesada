44f959a07d6c2588a6af0b724cde7149ec49aa79
FelipeQH29 <fquesada10287@ufide.ac.cr>
2025-07-16 20:55:27 -0600

    semana10
    RRRRRRRR

---
+++ Tienda_Felipe/src/main/java/com/tienda/service/UsuarioDetailsService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.tienda.service;
+
+/**
+ *
+ * @author erick
+ */
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UsuarioDetailsService {
+    public UserDetails 
+        loadUserByUsername(String username)
+                throws UsernameNotFoundException;
+}
+ 
